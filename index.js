34488f86638fc680e46fe05709ab59b593a6d65d
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-04-23 20:54:30 -0600

    env

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config()
 
 
 // ROUTES
+
+
+
+
